1ce125f99f1304adf41cf450e38c86141aec416b
kushagraagarwal19 <kushagra.agarwal@outlook.com>
2018-04-22 09:23:52 -0400

    Chnaged

--- index.js
+++ index.js
@@ -1,6 +1,3 @@
-// console.log('hello');
-
-
 var express = require('express');
 var bodyParser = require('body-parser');
 var path = require('path');
@@ -8,48 +5,40 @@ var path = require('path');
 var app = express();
 var expressValidator = require('express-validator')
 
-// var logger = function(req,res,next){
-//     console.log('Logging..')
-//     next()
-// }
-
-// app.use(logger)
+var amqp = require('amqplib/callback_api');
 
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
 
-
 // Body Parser Middleware
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended:false}));
-
-
+app.use(bodyParser.urlencoded({ extended: false }));
 // Express Validator Middleware
-app.use(expressValidator()); 
-
+app.use(expressValidator());
 
 // Global Variables
-app.use(function(req,res,next){
+app.use(function (req, res, next) {
     res.locals.errors = null;
     next();
 })
 
-app.get('/',function(req,res){
+app.get('/', function (req, res) {
     // var title = 'Customers'
     res.render('index', {
         title: 'Customers'
     });
 })
 
-
-app.post('/users/add',function(req,res){
+app.post('/users/add', function (req, res) {
     req.checkBody('first_name', 'First Name is required').notEmpty();
-    req.checkBody('last_name', 'Last Name is required').notEmpty();
+    req.checkBody('birthday', 'Birthday is required').notEmpty();
     req.checkBody('email', 'Email is required').notEmpty();
+    req.checkBody('cell', 'Mobile is required').notEmpty();
+    req.checkBody('password', 'Password is required').notEmpty();
 
     var errors = req.validationErrors();
 
-    if (errors){
+    if (errors) {
         res.render('index', {
             title: 'Customers',
             errors: errors
@@ -59,15 +48,29 @@ app.post('/users/add',function(req,res){
     else {
         console.log('Success');
         var newUser = {
-            firstName : req.body.first_name,
-            lastName : req.body.last_name,
-            email : req.body.email
+            firstName: req.body.first_name,
+            birthday: req.body.birthday,
+            email: req.body.email,
+            cell: req.body.cell,
+            password: req.body.password
         }
+        var q = 'q'
+        var url = process.env.CLOUDAMQP_URL || "amqp://spbdyewq:0PfDzWG1GiXAe5pNVv4u6WPdl9qF60cf@eagle.rmq.cloudamqp.com/spbdyewq";
+        var open = require('amqplib').connect(url);
+        open.then(function (conn) {
+            var ok = conn.createChannel();
+            ok = ok.then(function (ch) {
+                ch.assertQueue(q);
+                ch.sendToQueue(q, new Buffer(JSON.stringify(newUser)))
+            });
+            return ok;
+        }).then(null, console.warn);
         console.log(newUser)
+
+        // Consumer
     }
-    
 })
 
-app.listen(process.env.PORT || 5000, function(){
-    console.log("Server started on port 3000")
+app.listen(process.env.PORT || 3000, function () {
+    // console.log("Server started on port 3000")
 })
