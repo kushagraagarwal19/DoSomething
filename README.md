8b68057fcd1ef8a9498462875d43edf7c75c50f3
kushagraagarwal19 <kushagra.agarwal@outlook.com>
2018-04-23 07:20:09 -0400

    Changes

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # DoSomething
+
+https://www.raymondcamden.com/2017/02/08/using-social-login-with-passport-and-node/
+https://github.com/michaelkrukov/heroku-python-script
--- index.js
+++ index.js
@@ -1,72 +1,94 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-var path = require('path');
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
 
-var app = express();
-var expressValidator = require('express-validator')
+const app = express();
+const expressValidator = require('express-validator');
 
-var amqp = require('amqplib');
+const amqp = require('amqplib');
 
-app.set('view engine', 'ejs')
-app.set('views', path.join(__dirname, 'views'))
+// Global Variables
+app.use(function (req, res, next) {
+  res.locals.errors = null;
+  next();
+});
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 
 // Body Parser Middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+
 // Express Validator Middleware
 app.use(expressValidator());
 
-// Global Variables
-app.use(function (req, res, next) {
-    res.locals.errors = null;
-    next();
-})
-
+// Page Load Handler
 app.get('/', function (req, res) {
-    // var title = 'Customers'
-    res.render('index', {
-        title: 'Customers'
-    });
-})
+  res.render('index', {
+    title: 'Customers',
+  });
+});
 
+
+const q = 'UserRegistrationQueue2';
+const url = (process.env.CLOUDAMQP_URL || 'amqp://spbdyewq:0PfDzWG1GiXAe5pNVv4u6WPdl9qF60cf@eagle.rmq.cloudamqp.com/spbdyewqT') + '?heartbeat=20';
+// url += + '?heartbeat=20';
+
+// Form Submit Handler
 app.post('/users/add', function (req, res) {
-    req.checkBody('first_name', 'First Name is required').notEmpty();
-    req.checkBody('birthday', 'Birthday is required').notEmpty();
-    req.checkBody('email', 'Email is required').notEmpty();
-    req.checkBody('cell', 'Mobile is required').notEmpty();
-    req.checkBody('password', 'Password is required').notEmpty();
+  req.checkBody('first_name', 'First Name is required').notEmpty();
+  req.checkBody('birthday', 'Birthday is required').notEmpty();
+  req.checkBody('email', 'Email is required').notEmpty();
+  req.checkBody('cell', 'Mobile is required').notEmpty();
+  req.checkBody('password', 'Password is required').notEmpty();
 
-    var errors = req.validationErrors();
+  const errors = req.validationErrors();
 
-    if (errors) {
-        res.render('index', {
+  if (errors) {
+    res.render('index', {
+      title: 'Customers',
+      errors: errors,
+    });
+  }
+  else {
+    console.log('Success for form validation');
+    const newUser = {
+      firstName: req.body.first_name,
+      birthday: req.body.birthday,
+      email: req.body.email,
+      cell: req.body.cell,
+      password: req.body.password,
+    };
+
+    amqp.connect(url).then(function (conn) {
+      return conn.createChannel().then(function (ch) {
+        const ok = ch.assertQueue(q);
+        return ok.then(function () {
+          ch.sendToQueue(q, Buffer.from(JSON.stringify(newUser)));
+          return ch.close();
+        }).catch(function (error) {
+          // console.log('error123');
+          res.render('error', {
             title: 'Customers',
-            errors: errors
+            errors: error,
+          });
         });
-    }
-    else {
-        console.log('Success');
-        var newUser = {
-            firstName: req.body.first_name,
-            birthday: req.body.birthday,
-            email: req.body.email,
-            cell: req.body.cell,
-            password: req.body.password
-        }
-        var q = 'q'
-        var url = process.env.CLOUDAMQP_URL || "amqp://spbdyewq:0PfDzWG1GiXAe5pNVv4u6WPdl9qF60cf@eagle.rmq.cloudamqp.com/spbdyewq";
-        amqp.connect(url).then(function (conn) {
-            return conn.createChannel().then(function (ch) {
-                var q = 'q';
-                var ok = ch.assertQueue(q);
-                return ok.then(function (_qok) {
-                    ch.sendToQueue(q, new Buffer(JSON.stringify(newUser)))
-                    return ch.close();
-                });
-            }).finally(function () { conn.close(); });
-        });
-    }
-})
+      }).finally(function () {
+        conn.close();
+        res.render('added');
+      });
+    }).catch(function (error) {
+      // console.log('error345');
+      // console.log(error);
+      res.render('error', {
+        title: 'Customers',
+        errors: error,
+      });
+    });
+  }
+});
+
 app.listen(process.env.PORT || 3000, function () {
-    // console.log("Server started on port 3000")
-})
+  // console.log('Server started on port 3000')
+});
